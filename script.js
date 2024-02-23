6db4c62618f8b040e297d5a08825a0b5e56f267a
SHAHIL AHMAD <123900658+shahilahmad786@users.noreply.github.com>
2024-02-24 00:40:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let tablinks = document.getElementsByClassName('tab-links');
+let tabcontents = document.getElementsByClassName('tab-contents');
+
+function opentab(tabname) {
+	for (const tablink of tablinks) {
+		tablink.classList.remove("active-link")
+	}
+	for (const tabcontent of tabcontents) {
+		tabcontent.classList.remove("active-tab")
+	}
+	event.currentTarget.classList.add("active-link");
+	document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+
+let sidemenu = document.getElementById('sidemenu');
+
+function openmenu() {
+	sidemenu.style.right = '0';
+}
+function closemenu() {
+	sidemenu.style.right = '-200px';
+}
